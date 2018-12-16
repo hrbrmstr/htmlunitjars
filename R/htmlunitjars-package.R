eea9a4ffa090850f62aac2c529903100aa14103a
boB Rudis <bob@rud.is>
2018-12-16 07:34:42 -0500

    initial commit

--- R/htmlunitjars-package.R
+++ R/htmlunitjars-package.R
@@ -1,12 +1,12 @@
-#' ...
-#' 
-#' - URL: <https://gitlab.com/hrbrmstr/htmlunitjars>
-#' - BugReports: <https://gitlab.com/hrbrmstr/htmlunitjars/issues>
-#' 
+# Java Archive Wrapper Supporting the 'htmlunit' Package
+#'
+#' Contents of the 'HtmlUnit' & supporting Java archives <http://htmlunit.sourceforge.net/>
+#'
+#' Library JavaDoc: <http://htmlunit.sourceforge.net/apidocs/index.html>
+#'
 #' @md
 #' @name htmlunitjars
 #' @docType package
 #' @author Bob Rudis (bob@@rud.is)
-#' @import httr
-#' @importFrom jsonlite fromJSON
+#' @import rJava
 NULL
