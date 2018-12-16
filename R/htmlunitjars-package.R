ec9bec0016865e89faef2e9c0a8fa2d4b4011739
boB Rudis <bob@rud.is>
2018-12-16 07:03:40 -0500

    R package repo initialization complete

---
+++ R/htmlunitjars-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/htmlunitjars>
+#' - BugReports: <https://gitlab.com/hrbrmstr/htmlunitjars/issues>
+#' 
+#' @md
+#' @name htmlunitjars
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
